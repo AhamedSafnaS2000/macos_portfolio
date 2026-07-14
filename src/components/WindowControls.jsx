a347cdbbf302306f3c43e733df6765fc99c1b402
safnasNED25 <safnas2042ahamed@gmail.com>
2026-07-14 12:11:31 +0530

    done

--- src/components/WindowControls.jsx
+++ src/components/WindowControls.jsx
@@ -1,10 +1,11 @@
 import useWindowStore from "#store/window.js"
 
-const WindowControls = ({target}) => {
+const WindowControls = ({target, windowKey}) => {
   const {closeWindow} = useWindowStore();
+  const key = target ?? windowKey;
   return (
     <div id="window-controls">
-      <div className="close" onClick={() => closeWindow(target) }/>
+      <div className="close" onClick={() => closeWindow(key) }/>
       <div className="minimize" />
       <div className="maximize" />
     </div>
